fcbe6d0e91709fb10b19f70ba28c4c4494aa36db
titusrobin <143838819+titusrobin@users.noreply.github.com>
2023-10-11 13:19:10 -0400

    Update extract.py

--- mylib/extract.py
+++ mylib/extract.py
@@ -5,8 +5,8 @@ food dataset
 """
 import requests
 
-def extract(url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
-            file_path="data/GroceryDB_IgFPro.csv"):
+def extract(url="https://github.com/nickeubank/MIDS_Data/raw/master/US_AmericanCommunitySurvey/US_ACS_2017_10pct_sample.dta", 
+            file_path="american_community_survey.csv"):
     """"Extract a url to a file path"""
     with requests.get(url) as r:
         with open(file_path, 'wb') as f:
